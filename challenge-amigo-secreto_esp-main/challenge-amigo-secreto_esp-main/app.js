794c94c39c020a30d026983be64bf0132400c6cf
Zsaiy <zsaiy2016@gmail.com>
2025-03-10 21:27:51 -0500

    Se crea la funcion de sortear amigo y de mostrar el ganador, se da por terminado el proyecto

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -8,12 +8,24 @@ function limpiar_casilla(){
     let casilla = document.querySelector('#amigo');
     casilla.value='';
 }
+/* Funcion como lo pedia el trello.
+function asignar_nombre() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''; 
 
+    amigos.forEach(item => {
+        let li = document.createElement('li');
+        li.textContent = item;
+        lista.appendChild(li);
+    });
+}
+*/
 function asignar_nombre(nombre){
     let lista = document.getElementById('listaAmigos');
     let li = document.createElement('li');
     li.textContent = nombre;
     lista.append(li);
+    
 }
 
 function agregarAmigo(){
@@ -22,13 +34,30 @@ function agregarAmigo(){
     if (nombre==''){
         alert("Por favor, inserte un nombre.")
     }
-    else if(amigos.includes(nombre)){
+    else if(amigos.includes(nombre)){//Es para evitar nombres repetidos.
         alert("El nombre ya se encuentra en la lista, por favor intente nuevamente.")
     }
     else{
         amigos.push(nombre);
         asignar_nombre(nombre);
         limpiar_casilla();
+    }
+}
+
+function Sorteo(){
+    let indice = Math.floor(Math.random()*(amigos.length));
+    return indice
+}
+
+function sortearAmigo(){
+    let lista = document.querySelectorAll('#listaAmigos li');
+    if (lista.length == 0){
+        alert("La lista de amigos se encuentra vacia.")
+    }
+    else{
+        let resultado = document.getElementById('resultado');
+        let indiceGanador=Sorteo();
+        resultado.textContent="El elegido es: "+lista[indiceGanador].textContent;
     }
 }
 

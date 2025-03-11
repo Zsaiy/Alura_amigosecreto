dbaeb80c68f297d96d5dc6ae4de29002fc3085cf
Zsaiy <zsaiy2016@gmail.com>
2025-03-10 20:21:26 -0500

    Se crea el array, y dos funciones iniciales

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,2 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Capturar el input
+
 let amigos=[];
+
+function asignar_nombre(){
+    let lista = document.getElementById('listaAmigos');
+    amigos.forEach(item=>{
+        let li = document.createElement('li');
+        li.textContent = item;
+        lista.append(li);
+    })
+}
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    console.log(nombre);
+    if (nombre==''){
+        alert("Por favor, inserte un nombre.")
+    }
+    else{
+        amigos.push(nombre);
+        asignar_nombre();
+    }
+}
